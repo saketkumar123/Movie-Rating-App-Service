248cddfc72fc95529c99d1e32af5d37e7533d6ed
saketkumar123 <saket.ulsi@gmail.com>
2026-01-09 00:55:53 +0530

    Added RestTemplate Communication between Movie-Catalog-Service and movie-info-service

--- movie-catalog-service/src/main/java/com/chaiaurjava/movie_catalog_service/controller/MovieCatalogController.java
+++ movie-catalog-service/src/main/java/com/chaiaurjava/movie_catalog_service/controller/MovieCatalogController.java
@@ -1,20 +1,47 @@
 package com.chaiaurjava.movie_catalog_service.controller;
 
 import com.chaiaurjava.movie_catalog_service.models.CatalogItem;
+import com.chaiaurjava.movie_catalog_service.models.Movie;
+import com.chaiaurjava.movie_catalog_service.models.Rating;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogController {
 
+    RestTemplate restTemplate;
+
+    public MovieCatalogController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
     @GetMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable String userId){
-        return Collections.singletonList(new CatalogItem("Transformers", "Test", 4));
+
+
+
+        // get All rated movie Ids
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1234", 4),
+                new Rating("2345", 3)
+        );
+
+        return ratings. stream().map(rating -> {
+               Movie movie = restTemplate.getForObject("http://localhost:8081/movies/"+ rating.getMovieId(), Movie.class);
+            return new CatalogItem(movie.getName(), "Test", rating.getRating());
+        }).collect(Collectors.toList());
+
+
+        // For each moview Id, call movie info service and get details
+
+        // Put them all together
     }
 }
