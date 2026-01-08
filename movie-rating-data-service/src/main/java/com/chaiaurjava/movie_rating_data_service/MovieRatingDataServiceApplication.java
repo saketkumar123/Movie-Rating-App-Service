ae6c38492a4a62f04c3577417f8aac2188eb9018
saketkumar123 <saket.ulsi@gmail.com>
2026-01-08 23:00:46 +0530

    created springboot projects

---
+++ movie-rating-data-service/src/main/java/com/chaiaurjava/movie_rating_data_service/MovieRatingDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.chaiaurjava.movie_rating_data_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRatingDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRatingDataServiceApplication.class, args);
+	}
+
+}
---
+++ movie-rating-data-service/src/test/java/com/chaiaurjava/movie_rating_data_service/MovieRatingDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chaiaurjava.movie_rating_data_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRatingDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
