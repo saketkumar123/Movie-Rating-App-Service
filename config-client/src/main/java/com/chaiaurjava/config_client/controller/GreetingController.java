42a1415597b87f694b8a45202ec3c299ad003f86
saketkumar123 <saket.ulsi@gmail.com>
2026-01-14 22:58:49 +0530

    Updated with active profile

--- config-client/src/main/java/com/chaiaurjava/config_client/controller/GreetingController.java
+++ config-client/src/main/java/com/chaiaurjava/config_client/controller/GreetingController.java
@@ -4,11 +4,13 @@ import com.chaiaurjava.config_client.datasource.MySQLDatasource;
 import com.chaiaurjava.config_client.datasource.OracleDatasource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class GreetingController {
     private MySQLDatasource mySQLDatasource;
     private OracleDatasource oracleDatasource;
